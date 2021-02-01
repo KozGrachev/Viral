3a62389bd3b9f1e1712a6af298372ca01496e433
Ana <anasau28@gmail.com>
2021-02-01 16:03:12 +0000

    SocketIO connect, disconnect to room events

--- backend/index.ts
+++ backend/index.ts
@@ -1,9 +1,9 @@
 import express from 'express';
 import cors from 'cors';
-
 import { createServer } from 'http';
-import { Server} from 'socket.io';
-
+import { Server } from 'socket.io';
+import { userJoin, userLeave } from './utils/users';
+import { IUser } from './utils/users';
 const app = express();
 app.use(cors());
 
@@ -14,15 +14,57 @@ app.get('/', (_, res) => {
 
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
-  cors: { origin: 'http://localhost:3000', methods: ['GET', 'POST'] }
+  cors: { origin: 'http://192.168.1.26:3000', methods: ['GET', 'POST'] }
 });
 const PORT = 3001;
 
+interface GameState {
+
+}
+
 io.on('connection', (socket) => {
   console.log('server connected');
-  socket.on('hello', (data:string) => console.log(data)); 
-  socket.emit('hi back', 'hi hi hi'); 
+
+  socket.on('joinRoom', ({ username, room }: { username: string, room: string }) => {
+    const user = userJoin(socket.id, username, room);
+    socket.join(user.room);
+
+    // Welcome current user
+    socket.emit('joinConfirmation', `welcome ${username}, you can start playing now.`);
+
+    // Broadcast when a user connects
+    socket.broadcast
+      .to(user.room)
+      .emit(
+        'joinConfirmation',
+        `${username} has joined the game`);
+
+  });
+
+
+  socket.on('changeState', ({ user, state }: { user: IUser, state: GameState }) => {
+    socket.broadcast.to(user.room).emit('updatedState', state);
+    // will go and save this to redis 
+  });
+
+  //   socket.emit will send back message to sender only,
+  // io.emit will send message to all the client including sender
+  // if you want to send message to all but not back to sender then socket.broadcast.emit
+
+  // Runs when client disconnects
+  socket.on('disconnect', () => {
+    console.log('disconnect works');
+    const user = userLeave(socket.id);
+
+    if (user) {
+      io.to(user.room).emit(
+        'userLeft', `${user.username} has left the game`);
+    }
+  });
+
 });
+
+
 
 httpServer.listen(PORT, () =>
   console.log(`Server is listening on port ${PORT}`)
--- client/src/backend-dummy-client/dummy-client.tsx
+++ client/src/backend-dummy-client/dummy-client.tsx
@@ -1,7 +1,80 @@
+
+//based on change of state 
 import io from "socket.io-client";
 
-const socket = io('http://localhost:3001')
+const socket = io('http://192.168.1.26:3001')
 
-socket.emit('hello', 'hello')
+const fakeUser = { username: 'Maria', room: '1' }
 
-socket.on('hi back', (data: string) => console.log(data))
+
+
+
+export const sendChangedStateToBE = (state: GameStatedummy): void => {
+  socket.emit('changeState', { fakeUser, state })
+
+}
+interface GameStatedummy {
+
+}
+
+// //add the thunk 
+// const addTodoStarted = (STATE) => ({
+//   type: 'UPDATE_STORE', 
+//   payload:STATE, 
+// });
+
+// //add todo will be called from the component
+// export const addTodo = (STATE) => {
+//   return (dispatch) => {
+//     dispatch(addTodoStarted(STATE));
+
+
+// socket.on('updatedState', (state:GameStatedummy) => {
+
+// addTodo(state); 
+
+//   //state will be the payload 
+//   //trigger redux with middleware dispatch 
+// })
+
+
+export const joinRoom = (username: string, room: string) => {
+
+  // ultimately will passed on or read from the url  with 
+  // const { username, room } = Qs.parse(location.search, {
+  //   ignoreQueryPrefix: true
+  // });
+
+  socket.emit('joinRoom', { username, room });
+
+}
+
+// Message from server
+socket.on('joinConfirmation', (message: string) => {
+  console.log(message);
+});
+
+
+// on click => 
+joinRoom(fakeUser.username, fakeUser.room);
+
+// export const leaveRoom = () => {
+//   socket.emit('disconnect')
+// }
+//
+//onclick => 
+
+
+socket.on('userLeft', (message: string) => console.log(message))
+// leaveRoom ();
+
+
+// sending the state to the backend
+// socket.emit('Gamestate', {state:IState})
+
+// //getting the state from the 
+// socket.on('gamestate', (state:any) => {
+//   dispatch({type:UpdateState, payload:state)
+// })
+
+// Join chatroom
