02a2382689218e370ee04b79e15b2593c8e243ed
Konstantin Grachev <kgrachev@gmail.com>
2021-02-05 18:07:01 +0000

    feat(client): start connecting Redux to component CureDeck

--- client/src/components/CureDeck/CureDeck.tsx
+++ client/src/components/CureDeck/CureDeck.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import "./CureDeck.css"
+import { DefaultRootState, useSelector } from 'react-redux'
+import { RootState } from '../../redux/gameState/store';
+
 
 export interface CureDeckProps {
-  
+
   hasACureForMisinformationA: boolean,
   hasACureForMisinformationB: boolean,
   hasACureForMisinformationC: boolean,
@@ -10,19 +13,22 @@ export interface CureDeckProps {
 }
 
 
-export const CureDeck: React.FC<CureDeckProps> = ({  hasACureForMisinformationA
-  , hasACureForMisinformationB, hasACureForMisinformationC }: CureDeckProps) => { // SVGIcon
-// using redux instead of props for real implementation
-  //const gamestate = useSelector(state => state.gamestate) ...
 
-let noCureString = "No cure - it´s the end"
-let cureString = "u have a cure bitches!"
+export const CureDeck: React.FC<CureDeckProps> = ({ hasACureForMisinformationA
+  , hasACureForMisinformationB, hasACureForMisinformationC }: CureDeckProps) => { // SVGIcon
+  // using redux instead of props for real implementation
+  // const gamestate = useSelector((state: RootState) => state.GameState.connectionDeck)
+
+
+
+  let noCureString = "No cure - it´s the end"
+  let cureString = "u have a cure bitches!"
   const CureDeck = (<>
-    <div className="misinformation-cure-deck-container A">{hasACureForMisinformationA? cureString: noCureString }</div>
-    <div className="misinformation-cure-deck-container B">{hasACureForMisinformationB? cureString: noCureString}</div>
-    <div className="misinformation-cure-deck-container C">{hasACureForMisinformationC? cureString: noCureString}</div>
-  
-  
+    <div className="misinformation-cure-deck-container A">{hasACureForMisinformationA ? cureString : noCureString}</div>
+    <div className="misinformation-cure-deck-container B">{hasACureForMisinformationB ? cureString : noCureString}</div>
+    <div className="misinformation-cure-deck-container C">{hasACureForMisinformationC ? cureString : noCureString}</div>
+
+
   </>)
 
   return (
