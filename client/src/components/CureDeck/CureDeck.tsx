429d3966a37d8664754504e41ebba05d8e4c7bda
tillschmidt <schmidt.till.mail@gmail.com>
2021-02-06 19:19:59 +0100

    feat: cureDeck fully connected to redux

--- client/src/components/CureDeck/CureDeck.tsx
+++ client/src/components/CureDeck/CureDeck.tsx
@@ -4,29 +4,26 @@ import { DefaultRootState, useSelector } from 'react-redux'
 import { RootState } from '../../redux/gameState/store';
 
 
-export interface CureDeckProps {
-
-  hasACureForMisinformationA: boolean,
-  hasACureForMisinformationB: boolean,
-  hasACureForMisinformationC: boolean,
-
-}
 
 
 
-export const CureDeck: React.FC<CureDeckProps> = ({ hasACureForMisinformationA
-  , hasACureForMisinformationB, hasACureForMisinformationC }: CureDeckProps) => { // SVGIcon
+
+export const CureDeck: React.FC = ():JSX.Element => { // SVGIcon
   // using redux instead of props for real implementation
-  const gamestate = useSelector((state: RootState) => state.GameState.connectionDeck)
+  const gamestate = useSelector((state: RootState) => state.GameState)
+  let misinformationCommunityHasDebunked = gamestate.misinformation.community.debunked
+  let misinformationRelationsHasDebunked = gamestate.misinformation.relations.debunked
+  let misinformationSocialHasDebunked = gamestate.misinformation.social.debunked
 
 
 
-  let noCureString = "No cure - we're screwed"
-  let cureString = "We have a cure bitches!"
+
+  let noCureString = "Not debunked yet - we're screwed"
+  let cureString = "Debunked!! bitches!"
   const CureDeck = (<>
-    <div className="misinformation-cure-deck-container A">{hasACureForMisinformationA ? cureString : noCureString}</div>
-    <div className="misinformation-cure-deck-container B">{hasACureForMisinformationB ? cureString : noCureString}</div>
-    <div className="misinformation-cure-deck-container C">{hasACureForMisinformationC ? cureString : noCureString}</div>
+    <div className="misinformation-cure-deck-container A">{misinformationCommunityHasDebunked ? cureString : noCureString}</div>
+    <div className="misinformation-cure-deck-container B">{misinformationRelationsHasDebunked ? cureString : noCureString}</div>
+    <div className="misinformation-cure-deck-container C">{misinformationSocialHasDebunked ? cureString : noCureString}</div>
 
 
   </>)
