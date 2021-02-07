6882acd0a9d5b8bbc4549b8e605cc4a835a06d2d
Chris Owen <c.ornine@gmail.com>
2021-02-07 19:02:46 +0000

    FEAT: add toKebabCase to utils for CSS class names

--- client/src/helpers/utils.ts
+++ client/src/helpers/utils.ts
@@ -6,3 +6,11 @@ export const toCamelCase = (str: string): string =>
 export const toPascalCase = (str: string): string =>
   str
     .replace(/\w\S*/g, (letter) => letter.charAt(0).toUpperCase() + letter.substr(1).toLowerCase());
+
+export const toKebabCase = (str: string): string => {
+  const words = str.match(/[A-Z]{2,}(?=[A-Z][a-z0-9]*|\b)|[A-Z]?[a-z0-9]*|[A-Z]|[0-9]+/g);
+  return words
+    .filter(Boolean)
+    .map(x => x.toLowerCase())
+    .join('-') ?? '';
+  }
