a2b4753a9586d86eed2fa86eeda2cf81132ea622
Connor MacPhee <cmacphee66@gmail.com>
2021-02-08 18:37:23 +0000

    feat: few changes

---
+++ client/src/components/NewGameMenu/NewGameMenu.stories.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+// import { Story, Meta } from '@storybook/react/types-6-0';
+// import { NewGameMenu } from './NewGameMenu';
+// import { withDesign } from 'storybook-addon-designs';
+
+// export default {
+//   title: 'New Game Menu',
+//   component: NewGameMenu,
+//   decorators: [withDesign],
+// } as Meta;
+
+// const Template: Story<COMPONENTPROPS> = (args: COMPONENTPROPS) => (
+//   <COMPONENT {...args} />
+// );
+
+// export const StoryName = Template.bind({});
+// StoryName.args = {
+//   // PROP: VALUE
+// };
