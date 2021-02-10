9f3ba992d95f8f85d2a91c023adf0e42bd8a20c3
Ana <anasau28@gmail.com>
2021-02-10 10:57:20 +0000

    get cards button styled

--- client/src/components/GameOn.tsx
+++ client/src/components/GameOn.tsx
@@ -68,6 +68,17 @@ export const GameOn: React.FC<Props> = ({ rendered }): JSX.Element => {
             <div className="sidebar-left">
               {(checkCards(state) === true) ?
                 <button
+                  style={{
+                    background: 'royalblue', 
+                    padding: ' 15px 35px', 
+                    fontSize: '1em', 
+                    borderRadius: '20px', 
+                    border: 'none', 
+                    fontWeight: 'bold', 
+                    color: 'white', 
+                    margin: '15px auto',
+                    width: '100%'
+                  }}
                   onClick={getCards}> GET CARDS </button>
                 :
                 <CardHand />
