cab0444d20e6b6c83bacb835fd3ad65d3e2921ad
Ana <anasau28@gmail.com>
2021-02-18 20:50:39 +0000

    redis update

--- backend/utils/game.js
+++ backend/utils/game.js
@@ -1,3 +1,3 @@
-"use strict";
+'use strict';
 //? put obljects here as we use/store stuff
-Object.defineProperty(exports, "__esModule", { value: true });
+Object.defineProperty(exports, '__esModule', { value: true });
