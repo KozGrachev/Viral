8c31846cd6f3bfcfc91904b7e749d4f5ecce6afa
Chris Owen <c.ornine@gmail.com>
2021-02-21 00:29:12 +0000

    feat(curedeck tsx & scss): add classes to cureDeck to display debunked status visually

--- client/src/components/CureDeck/CureDeck.tsx
+++ client/src/components/CureDeck/CureDeck.tsx
@@ -6,13 +6,13 @@ import { RootState } from '../../redux/gameState/store';
 export const CureDeck: React.FC = ():JSX.Element => {
   const { community, relations, social } = useSelector((state:RootState) => state.gameStateReducer.misinformation)
   const CureDeck = (<>
-    <div className="debunk-indicator community">
+    <div className={`debunk-indicator community ${community.debunked ? 'debunked' : ''}`}>
       {community.markersLeft}
     </div>
-    <div className="debunk-indicator relations">
+    <div className={`debunk-indicator relations ${relations.debunked ? 'debunked' : ''}`}>
       {relations.markersLeft}
     </div>
-    <div className="debunk-indicator social">
+    <div className={`debunk-indicator social ${social.debunked ? 'debunked' : ''}`}>
       {social.markersLeft}
     </div>
   </>)
--- client/src/components/Source/Source.tsx
+++ client/src/components/Source/Source.tsx
@@ -118,7 +118,7 @@ export const SourceComponent: React.FC<SourceProps> = ({ source }: SourceProps) 
   let canLogOnClassName = canLogOn ? 'can-log-on' : ''
   let canDebunkClassName = canDebunk ? 'can-debunk' : ''
 
-  function unclickableMessage () {return null} //? whats this?
+  function unclickableMessage () {return null}
 
   const closeModal = () => {
     setIsOpen(false)
